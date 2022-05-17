6d327b220ec916e5fd015b7c81e0208bf9219a7d
Davner <davner23@gmail.com>
2022-05-17 02:24:03 -0400

    Atualização 17/05/2022

---
+++ public/js/about.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let movie_id = location.pathname;
+
+//fetching movie details
+fetch(`${movie_detail_http}${movie_id}?` + new URLSearchParams({
+    api_key: api_key
+}))
+
+.then(res => res.json())
+.then(data => {
+    setupMovieInfo(data);
+})
