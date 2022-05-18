8cd1f714bc94a479490735920d781c8247adf18f
Davner <davner23@gmail.com>
2022-05-18 02:53:18 -0400

    Nova atualização

--- public/js/api.js
+++ public/js/api.js
@@ -1,6 +1,6 @@
-let api_key = "25986140e0d34941636f99dbf6f5310e";
-let img_url = "https://image.tmdb.org/t/p/w500";
-let original_img_url = "https://image.tmdb.org/t/p/original";
-let genres_list_http = "https://image.tmdb.org/3/genre/movie/list?";
-let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
-let movie_detail_http = "https://api.themoviedb.org/3/movie";
+let api_key = "729bb2f771bb0d7b1578127952f4c446";
+let img_url = "https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg";
+let genres_list_http = "https://api.themoviedb.org/3/discover/movie?";
+// let original_img_url = "https://api.themoviedb.org/3/genre/movie/list?";
+// let movie_genres_http = "https://api.themoviedb.org/3/genre/movie/list?";
+// let movie_detail_http = "https://api.themoviedb.org/3/list/{list_id}?";
